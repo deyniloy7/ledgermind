9baa970c41ba43fd1a9b58e921127ccf0d2a9bff
deyniloy7 <dey.niloy007@gmail.com>
2026-08-25 18:55:37 +0530

    feat: add ClaudeProvider constructor with AsyncAnthropic client

--- extraction/providers.py
+++ extraction/providers.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from anthropic import AsyncAnthropic
 from extraction.schemas import ExtractedInvoice
 
 
@@ -6,3 +7,8 @@ class LLMProvider(ABC):
     @abstractmethod
     async def extract_invoice(self, file_bytes: bytes) -> ExtractedInvoice:
         pass
+
+
+class ClaudeProvider(LLMProvider):
+    def __init__(self, api_key: str):
+        self.client = AsyncAnthropic(api_key=api_key)
